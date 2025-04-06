1f3c54302b90f63340489492e2ee912723efb078
bmulaw <bmulaw@bu.edu>
2025-04-05 21:39:37 -0400

    react cleanup + organizing folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 import Form from './components/lyrics'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
           {"\u00f3n"}
           <Form/>
-        </a>
-        
+        </p>
       </header>
     </div>
   );
