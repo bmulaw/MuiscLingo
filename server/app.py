ecf3556c7cc1ff2c5dea354e4d07b7f7630a07ef
bmulaw <bmulaw@bu.edu>
2025-04-05 21:14:39 -0400

    init

---
+++ server/app.py
@@ -0,0 +1,4 @@
+# 1. Set up Spotify music player
+# 2. Request lyrics for a playing song
+# 3. Request google translate for each line of lyric
+# 4. Respond with each line of lyrics in English & French
